9e7445b0f2c02d3db69e633d7cf16c04bff4b753
Ross Brandon <ross.brandon@pm.me>
2023-11-05 13:06:03 -0600

    mockup of status cards

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
@@ -14,33 +14,23 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<strong class="text-xl">Data Flora</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="default">
+				<input
+					class="input w-full"
+					title="Tenant Search"
+					type="text"
+					placeholder="Search for a tenant..."
+				/>
+			</svelte:fragment>
+			<svelte:fragment slot="headline">
+				<span>Upstream A</span>
+				<span class="text-xl mx-2">::</span>
+				<span>Acme</span>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
+				<LightSwitch />
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
---
+++ src/routes/detail/+page.svelte
@@ -0,0 +1,7 @@
+<div class="container h-full mt-20 mx-auto flex justify-center">
+	<div class="space-y-10 text-center flex flex-col items-center">
+		<div class="flex justify-center space-x-5">
+			<h1>Detail Page</h1>
+		</div>
+	</div>
+</div>
